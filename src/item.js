b93e3bb769173841d03204d7c3bef5e6174a2ce6
Eason Chen <e.chen@glowing.com>
2019-04-10 15:09:22 +0800

    Fix timestamp error

--- src/item.js
+++ src/item.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const now = new Date();
-
 class Item {
   constructor(options = {}) {
     this._id = options.id;
-    this._date = now.toDateString();
-    this._timestamp = now.getTime();
+    this._date = new Date().toDateString();
+    this._timestamp = new Date().getTime();
     this.description = options.description;
     this.isStarred = options.isStarred || false;
     this.boards = options.boards || [];
